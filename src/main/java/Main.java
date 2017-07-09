42f68c6499104c221e4330b622b861ae7819c342
coolcom200 <leonfattakhov@yahoo.com>
2017-07-08 21:29:18 -0400

    Added Method to add Student

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,32 @@
 import java.io.IOException;
+import java.text.DateFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by leon on 7/8/2017.
  */
 public class Main {
+    public static void createStudent(IDriver driver,String number,String club){
+        driver.setChannel("https://clubattendancesjam.firebaseio.com/"+number+"/"+club);
+        Map<String, String> data = new HashMap<String, String>();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        data.put("Meeting",dateFormat.format(date));
+        boolean responseCode = driver.write(data);
+    }
     public static void main(String[] args) throws IOException {
         IDriver driver = new Driver();
+        createStudent(driver,"789789789","Computer_Science");
         Map<String, String> data = new HashMap<String, String>();
-        data.put("name", "ll");
-        data.put("user", "Loser");
-        driver.setChannel("https://clubattendancesjam.firebaseio.com/123456798/user");
-        boolean responseCode = driver.write(data);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        data.put("Meeting",dateFormat.format(date));
+        driver.setChannel("https://clubattendancesjam.firebaseio.com/"+"789789789"+"/Science");
+        driver.write(data);
+
+
     }
 }
