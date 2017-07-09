155d6fd3b1a5ad451a31bd27352e67bd442e31c6
coolcom200 <leonfattakhov@yahoo.com>
2017-07-08 21:05:28 -0400

    Updated Project to use Maven

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by leon on 7/8/2017.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        IDriver driver = new Driver();
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("name", "ll");
+        data.put("user", "Loser");
+        driver.setChannel("https://clubattendancesjam.firebaseio.com/123456798/user");
+        boolean responseCode = driver.write(data);
+    }
+}
