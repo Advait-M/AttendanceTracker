4aaa08240e11f737370b0f7f4d8d1cfee402e8ae
coolcom200 <leonfattakhov@yahoo.com>
2017-09-26 16:46:04 -0400

    added backup script

---
+++ puller.py
@@ -0,0 +1,31 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import os
+import json
+from datetime import datetime
+
+cred = credentials.Certificate('XXXXXX.json')
+firebase_admin.initialize_app(cred, {
+	'databaseURL': 'https://clubattendancesjam.firebaseio.com'
+})
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference().get()
+allFiles = os.listdir("PATH")
+max_date = 0
+min_date = float("inf")
+total_backups = 0
+for i in allFiles:
+	if "CAbackup" in i:
+		total_backups += 1
+		cur_date = float(i.split("CAbackup")[0].replace("_", "."))  # will be in epoch
+		if cur_date > max_date:
+			max_date = cur_date
+		if cur_date < min_date:
+			min_date = cur_date
+if total_backups > 5:
+	os.remove("PATH" + str(min_date).replace(".", "_") + "CAbackup.json")
+	print("deleted", str(min_date).replace(".", "_") + "CAbackup.json")
+with open(str(datetime.now().timestamp()).replace(".", "_") + 'CAbackup.json', 'w+') as outfile:
+	json.dump(ref, outfile)
