71446fb2aae360427b35905bc5d3664e65946076
coolcom200 <leonfattakhov@yahoo.com>
2017-10-02 19:17:57 -0400

    hotfix for sign in with paid present

--- src/main/java/Firebase.java
+++ src/main/java/Firebase.java
@@ -101,11 +101,14 @@ public class Firebase {
         }
         Set<Map.Entry<String, JsonElement>> entries = element.getAsJsonObject().entrySet(); //will return members of your object
         String MaxKey;
+        int MaxMeeting = 0;
         if (getPaid(number, club)) {
-            MaxKey = "meeting" + (entries.size()- 1);
+            MaxMeeting = (entries.size()- 1);
+            MaxKey = "meeting" + MaxMeeting;
         }
         else {
-            MaxKey = "meeting" + (entries.size()); // Sets current meeting day that is in the database
+            MaxMeeting = entries.size();
+            MaxKey = "meeting" + MaxMeeting; // Sets current meeting day that is in the database
         }
 
         JsonElement MaxValue = null; // this will be assigned when the MaxKey is found
@@ -128,7 +131,7 @@ public class Firebase {
         // Checks if max value is today if not then signs user in
         if (!(MaxValueString.equals(date))) {
             // Sign user in for  today
-            data.put("meeting" + (entries.size() + 1), getDate());
+            data.put("meeting" + (MaxMeeting+1), getDate());
             this.driver.setChannel(number, club);
             boolean result = this.driver.write(data);
             if (result) {
@@ -209,31 +212,31 @@ public class Firebase {
         return setUserPaid(number, club) == setUserPaid("club_master", club);
     }
 
-    public static void main(String[] args) {
-        Firebase firebase = new Firebase();
-        firebase.addMeetingDay("641852934", "science");
-        firebase.setPaid("641852934", "science");
-
-
-        Random r = new Random();
-
-        String[] clubs = new String[]{"Art Club Ideas", "Sculpture Club", "Photography Club", "Art History Club", "Drama Club Ideas ", "Shakespeare Club", "Classics Club", "Monologue Club", "Comedy Sportz Club", "Improv Club", "Film Club Ideas", "Foreign Film Club", "Screenwriting Club", "Directing Club", "48-Hour Film Festival Club", "Science Club Ideas", "Future Scientists Club", "Marine Biology Club", "Future Medical Professionals Club", "Math Club Ideas", "Math Homework Club", "Pi Club", "Literature Club Ideas", "Literature Magazine Club", "Creative Writing Club", "Book Club", "Foreign Book Club", "History Club Ideas", "Ancient History Club", "Language Club Ideas", "Anime Club", "Chess Club", "Video Games Club", "Skiing Club", "Religion Club", "Adventure Club", "Charity Club Ideas", "Save Endangered Species Club"};
-        for (int i = 0; i < 100; i++) {
-            String data = String.valueOf(r.nextInt(900000000) + 100000000);
-            String club = clubs[r.nextInt(clubs.length)].replace(" ", "-");
-
-            firebase.addMeetingDay(data,club);
-            firebase.setPaid(data, club);
-
-        }
-        firebase.addMeetingDay("741852936", "science");
-        firebase.addMeetingDay("741852936", "computer-science");
-        firebase.addMeetingDay("641852934", "science");
-        firebase.addMeetingDay("641852934", "computer-science");
-        firebase.addMeetingDay("641852934", "mathletes");
-
-
-    }
+//    public static void main(String[] args) {
+//        Firebase firebase = new Firebase();
+//        firebase.addMeetingDay("641852934", "science");
+//        firebase.setPaid("641852934", "science");
+//
+//
+//        Random r = new Random();
+//
+//        String[] clubs = new String[]{"Art Club Ideas", "Sculpture Club", "Photography Club", "Art History Club", "Drama Club Ideas ", "Shakespeare Club", "Classics Club", "Monologue Club", "Comedy Sportz Club", "Improv Club", "Film Club Ideas", "Foreign Film Club", "Screenwriting Club", "Directing Club", "48-Hour Film Festival Club", "Science Club Ideas", "Future Scientists Club", "Marine Biology Club", "Future Medical Professionals Club", "Math Club Ideas", "Math Homework Club", "Pi Club", "Literature Club Ideas", "Literature Magazine Club", "Creative Writing Club", "Book Club", "Foreign Book Club", "History Club Ideas", "Ancient History Club", "Language Club Ideas", "Anime Club", "Chess Club", "Video Games Club", "Skiing Club", "Religion Club", "Adventure Club", "Charity Club Ideas", "Save Endangered Species Club"};
+//        for (int i = 0; i < 100; i++) {
+//            String data = String.valueOf(r.nextInt(900000000) + 100000000);
+//            String club = clubs[r.nextInt(clubs.length)].replace(" ", "-");
+//
+//            firebase.addMeetingDay(data,club);
+//            firebase.setPaid(data, club);
+//
+//        }
+//        firebase.addMeetingDay("741852936", "science");
+//        firebase.addMeetingDay("741852936", "computer-science");
+//        firebase.addMeetingDay("641852934", "science");
+//        firebase.addMeetingDay("641852934", "computer-science");
+//        firebase.addMeetingDay("641852934", "mathletes");
+//
+//
+//    }
 }
 
 
